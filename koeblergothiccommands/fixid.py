a762e06b3d44b319460c81dc4cf5857aa59f205c
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-14 02:11:18 +0200

    ci add command fixid to workflow circleci

--- koeblergothiccommands/fixid.py
+++ koeblergothiccommands/fixid.py
@@ -5,7 +5,16 @@ Write file.
 (This can't be done from lexibank script.)
 """
 
-def run():
+import csv
+
+def run(args):
     """
     """
-    pass
+    with open("cldf/forms.csv", "r") as f:
+        data = list(csv.reader(f))
+    with open("cldf/forms.csv", "w+") as f:
+        writer = csv.writer(f)
+        writer.writerow(data[0])  # headers
+        for i, row in enumerate(data[1:]):
+            row[0] = f"f{i+1}"
+            writer.writerow(row)
