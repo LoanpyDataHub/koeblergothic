d88377e500073db683f0610d30d4e55a65ea2d6e
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-05-08 15:56:44 +0200

    [skip ci] addcopybuttondocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+# # https://sphinx-copybutton.readthedocs.io/en/latest/index.html
+
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'koeblergothic'
@@ -10,9 +13,20 @@ copyright = '2023, Viktor Martinović'
 author = 'Viktor Martinović'
 version = '1.0'
 release = '1.0'
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_copybutton']
 html_theme = 'sphinx_rtd_theme'
-# TODO replace mock with docs/requirements.txt after loanpy-release
-autodoc_mock_imports = ["epitran", "loanpy", "lingpy", "clldutils", "tabulate",
-                        "attr", "pylexibank", "cldfbench", "spacy",
-                        "pycldf", "pysem", "tqdm"]
+
+autodoc_mock_imports = [
+    "attr",
+    "cldfbench",
+    "clldutils",
+    "lexibank_gerstnerhungarian",
+    "loanpy",
+    "lingpy",
+    "pycldf",
+    "pylexibank",
+    "pysem",
+    "spacy",
+    "tabulate",
+    "tqdm"
+]
